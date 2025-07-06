48827f38d225057ec14f265da3586660266b00f4
CafPatronato <CAFMCLCORSICO@GMAIL.COM>
2025-07-07 01:25:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import "@fullcalendar/common/main.css";
-import "@fullcalendar/timegrid/main.css";
-import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/index.css";
+import "@fullcalendar/daygrid/index.css";
 
 export default function App() {
   const vincoliFissi = [
